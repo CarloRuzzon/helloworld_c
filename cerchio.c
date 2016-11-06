e9ec62226dc8c2178ebc7ff35c089139f9bae37f
CarloRuzzon <carloruzzon@gmail.com>
2016-11-06 22:47:12 +0100

    risoluzione problemi

--- cerchio.c
+++ cerchio.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main() 
 {
-  int d, c, r;
-  float a; 
+  int  r;
+  float d, c, a; 
   printf("Inserire il raggio del cerchio: ");
   scanf("%d", &r);
   d = r * 2;
   a = r * r * 3.14159;
-  c = r * 2 * 3,14159;
-  printf("Il diametro e': %d * 2 = %d\n", r, d);
-  printf("L'area e': %d * %d * 3,14159 = %f\n", r, r, a);
-  printf("La circonferenza e': %d * 2 * 3,14159 = %d\n", r, c);
+  c = r * 2 * 3.14159;
+  printf("Il diametro e': %d * 2 = %f\n", r, d);
+  printf("L'area e': %d * %d * 3.14159 = %f\n", r, r, a);
+  printf("La circonferenza e': %d * 2 * 3.14159 = %f\n", r, c);
 return 0;
 }
