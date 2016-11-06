01565d1f70a961bfa7e164de519326d916a7960f
CarloRuzzon <carloruzzon@gmail.com>
2016-11-06 20:06:27 +0100

    iniziali.c

---
+++ iniziali.c
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+int main ()
+{
+  printf("          CCCCC\n");
+  printf("        C       C\n");
+  printf("       C         C\n");
+  printf("       C         C\n");
+  printf("       C         C\n");
+  printf("\n");
+  printf("\n");
+  printf("\n");
+  printf("        RRRRRRRRRRRR\n");
+  printf("              R    R\n");
+  printf("             R R  R\n");
+  printf("            R    R\n");
+  printf("          R\n");
+  printf("        R\n");
+
+
+
+
+
+
+
+
+
+
+
+printf("iniziali: C R\n");
+return 0;
+}
