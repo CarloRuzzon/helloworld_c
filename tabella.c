1dc7e7d4144f9f9f263a6335cc3272decbfc6258
CarloRuzzon <carloruzzon@gmail.com>
2016-11-21 19:57:30 +0100

    tabella

---
+++ tabella.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+int main(){
+int num=0;
+
+printf("%d    ", num);
+printf("  %d    ", num*num);
+printf("  %d\n", num*num*num);
+num++;
+
+printf("%d    ", num);
+printf("  %d    ", num*num);
+printf("  %d\n", num*num*num);
+num++;
+
+printf("%d    ", num);
+printf("  %d    ", num*num);
+printf("  %d\n", num*num*num);
+num++;
+
+printf("%d    ", num);
+printf("  %d    ", num*num);
+printf("  %d\n", num*num*num);
+num++;
+
+printf("%d    ", num);
+printf("  %d    ", num*num);
+printf("  %d\n", num*num*num);
+num++;
+
+printf("%d    ", num);
+printf("  %d    ", num*num);
+printf("  %d\n", num*num*num);
+num++;
+
+printf("%d    ", num);
+printf("  %d    ", num*num);
+printf("  %d\n", num*num*num);
+num++;
+
+printf("%d    ", num);
+printf("  %d    ", num*num);
+printf("  %d\n", num*num*num);
+num++;
+
+printf("%d    ", num);
+printf("  %d    ", num*num);
+printf("  %d\n", num*num*num);
+num++;
+
+
+printf("%d    ", num);
+printf("  %d    ", num*num);
+printf("  %d\n", num*num*num);
+num++;
+
+printf("%d    ", num);
+printf(" %d    ", num*num);
+printf(" %d\n", num*num*num);
+
+
+
+
+return 0;
+}
+
+
+
+
+
+
+
