b741ff87bef28df9d5e1eb4e0e249d4a8133bf39
Fabio Z Tessitore <fabioztessitore@gmail.com>
2016-11-21 21:10:55 +0100

    tabellina

---
+++ es2_31.c
@@ -0,0 +1,40 @@
+/* C
+   Deitel
+   Esercizio 2.31
+*/
+
+/* Stampa i quadrati e i cubi dei numeri da 0 a 10
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  int num = 0;
+
+  printf("numero\t\tquadrato\tcubo\n");
+  
+  printf("%d\t\t%d\t\t%d\n", num, num*num, num*num*num);
+  num = num + 1;
+  printf("%d\t\t%d\t\t%d\n", num, num*num, num*num*num);
+  num = num + 1;
+  printf("%d\t\t%d\t\t%d\n", num, num*num, num*num*num);
+  num = num + 1;
+  printf("%d\t\t%d\t\t%d\n", num, num*num, num*num*num);
+  num = num + 1;
+  printf("%d\t\t%d\t\t%d\n", num, num*num, num*num*num);
+  num = num + 1;
+  printf("%d\t\t%d\t\t%d\n", num, num*num, num*num*num);
+  num = num + 1;
+  printf("%d\t\t%d\t\t%d\n", num, num*num, num*num*num);
+  num = num + 1;
+  printf("%d\t\t%d\t\t%d\n", num, num*num, num*num*num);
+  num = num + 1;
+  printf("%d\t\t%d\t\t%d\n", num, num*num, num*num*num);
+  num = num + 1;
+  printf("%d\t\t%d\t\t%d\n", num, num*num, num*num*num);
+  num = num + 1;
+  printf("%d\t\t%d\t\t%d\n", num, num*num, num*num*num);
+
+  return 0;
+}
